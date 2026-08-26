010a173ebbaa64aec45de1a00d48affc0c14cf48
Nakshatra Jain <nakshatrajain246@gmail.com>
2026-08-27 02:37:57 +0530

    Time: 0 ms (100%), Space: 8.8 MB (71.26%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        unordered_map<char, string> charToS;
-        unordered_map<string, char> sToChar;
+        unordered_map<char, string> cMp;
+        unordered_map<string, char> sMp;
         int cur=0;
         for(char c: pattern){
             if(cur>=s.size()) return false;
-            int size=1;
-            while(cur+size<=s.size() && s[cur+size]!=' '){
-                size++;
+            int start=cur;
+            while(cur<s.size() && s[cur]!=' '){
+                cur++;
             }
-            string tmp =s.substr(cur, size);
-            if((charToS.count(c) && charToS[c]!=tmp) || (sToChar.count(tmp) && sToChar[tmp]!=c)) return false;
-            charToS[c]=tmp;
-            sToChar[tmp]=c;
-            cur=cur+size+1;
+            string tmp = s.substr(start, cur-start);
+            if(cMp.count(c) && cMp[c]!=tmp) return false;
+            if(sMp.count(tmp) && sMp[tmp]!=c) return false;
+            cMp[c]=tmp;
+            sMp[tmp]=c;
+            cur++;
         }
         return cur>=s.size();
     }
