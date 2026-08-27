50c516744001d9adb89a47e8d99075ef97e8c9a3
Nakshatra Jain <nakshatrajain246@gmail.com>
2026-08-28 00:02:08 +0530

    Time: 0 ms (100%), Space: 9.4 MB (18.39%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int left =0, right=matrix[0].size()-1, bottom=matrix.size()-1, top=0;
+        vector<int> ans;
+        while(left<=right && top<=bottom){
+            for(int i=left;i<=right;i++){
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++){
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+            for(int i=right;i>=left && top<=bottom;i--){
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+            for(int i=bottom;i>=top && left<=right;i--){
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+        return ans;
+    }
+};
